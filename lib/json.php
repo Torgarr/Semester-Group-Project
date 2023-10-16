4e11762be471693e124a12ec64c681c0c4ddf357
Dexter Walters <tyr.odinson123@gmail.com>
2023-10-16 00:00:18 -0400

    Fixed comments feature.

--- lib/json.php
+++ lib/json.php
@@ -18,7 +18,7 @@ function latestPost() {
         // HTML Output for the latest post
         ?>
         <div class="card mb-4">
-            <a href="#!"><img class="card-img-top" src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg" alt="..." /></a>
+            <a href="#!"><img class="card-img-top" src=".\\data\\rabbit.jpg" alt="Default image of a rabbit jumping! <3" /></a>
             <div class="card-body">
                 <div class="small text-muted"><?php echo $date; ?></div>
                 <h2 class="card-title h4"><?php echo $title; ?></h2>
@@ -44,12 +44,33 @@ function readPosts() {
         // HTML Output for each post
         ?>
         <div class="card mb-4">
-            <a href="#!"><img class="card-img-top" src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg" alt="..." /></a>
+            <a href="#!"><img class="card-img-top" src=".\\data\\rabbit.jpg" alt="Default image of a rabbit jumping! <3" /></a>
             <div class="card-body">
                 <div class="small text-muted"><?php echo $date; ?></div>
                 <h2 class="card-title h4"><?php echo $title; ?></h2>
                 <p class="card-text"><?php echo $post; ?></p>
             </div>
+        </div>
+        <?php
+    }
+}
+
+function readComments() {
+    // Read JSON file content
+    $json_data = file_get_contents('.\\data\\comments.json');
+
+    // Decode JSON data into PHP array
+    $data = json_decode($json_data, true);
+
+    // Loop through comments and print them line by line
+    foreach ($data['comments'] as $comment) {
+        $user = $comment['user'];
+        $commentText = $comment['comment'];
+
+        // HTML Output for each comment
+        ?>
+        <div class="card-body">
+            <strong><?php echo $user; ?>:</strong> <?php echo $commentText; ?><br>
         </div>
         <?php
     }
